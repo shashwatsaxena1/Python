4d4c764c2f64da5750bf2ed8dd1357d6a47d05ae
Shashwat <shashwatsaxena17@gmail.com>
2026-06-19 19:38:45 +0530

    Swapping

---
+++ Basics/Swap_two_numbers.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter the number 1: "));
+num2 = int(input("Enter the number 2: "));
+print(f"Before swapping {num1} and {num2}");
+num3 = num1
+num1 = num2
+num2 = num3
+print(f"After swapping {num1} and {num2}");

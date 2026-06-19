a854d7771ae0f8ef5e2636195fc327441c5f790c
Shashwat <shashwatsaxena17@gmail.com>
2026-06-19 13:30:16 +0530

    Basic Maths

---
+++ Basics/Multiply_Div_of_two.py
@@ -0,0 +1,4 @@
+num1 = int(input("Enter the number 1: "))
+num2 = int(input("Enter the number 2: "))
+print(f"The multiplication of {num1} and {num2} is {num1*num2}");
+print(f"The division of {num1} and {num2} is {num1//num2}");

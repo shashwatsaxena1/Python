9e97dff42f4dcc85c9e879ac825349f3a5614a62
Shashwat <shashwatsaxena17@gmail.com>
2026-06-19 12:56:53 +0530

    Basic

---
+++ Basics/Name_age.py
@@ -0,0 +1,10 @@
+name="Shashwat";
+age=18;
+print("Hello My Name Is ",name,"and I Am ",age,"Years Old");
+'''
+Formated String Method
+'''
+city="Lucknow";
+degree="B-Tech CSE";
+college="GLA University";
+print(f"I'm currently pursuing a {degree} from {college} and i am from {city}");
---
+++ Basics/Sum_two_numbers.py
@@ -0,0 +1,4 @@
+num1= int(input("Enter a number 1: "))
+num2= int(input("Enter a number 2: "))
+print(f"The sum of {num1} and {num2} is {num1+num2}");
+

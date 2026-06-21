98596c90947cf3af8bd75a8095ec22db22960fb8
Shashwat <shashwatsaxena17@gmail.com>
2026-06-21 23:48:09 +0530

    SI Calc

---
+++ Basics/Calculate_simple_interest.py
@@ -0,0 +1,5 @@
+principal = int(input("Enter the principal amount: "));
+intrest = int(input("Enter the intrest rate per annum: "));
+Time = int(input("Enter the time in years: "));
+
+print(f"The simple intrest is {principal*intrest*Time/100}");
